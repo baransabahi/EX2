9772373705728f6ae9238e5af1e26d24c5e1816d
baransabahi <117539124+baransabahi@users.noreply.github.com>
2022-11-16 08:43:08 -0800

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,46 @@
+alert("hello word");
+prompt ("bita");
+prompt ("hello tanks");
+document.write('<h1>bita torkashvand</hl>');
+document.getElementById('bb').innerHTML=" Class has starting";
+console.log('bita torkashvand');
+var date=Date();
+document.write(date + "<br>");
+var status;
+status = 5+2;
+document.write(status + "<br>");
+var hello ;
+hello = "techer";
+hello += "uni<br>torkashvand<br>bita" + "<br>";
+document.write(hello);  
+status == 7;
+document.write(status + "<br>");
+status ==12 ;
+document.write("true");
+var x= status;
+document.write(!(12>x)&&(x>7));
+function welcomeMsg(){
+    alert("welcom to JavaScript!");
+}
+welcomeMsg();
+function clasjavas(temp){
+    alert("i love javas");
+
+}
+clasjavas();
+document.write("Width" + "<br>") ; 
+var y=5;
+var x=y--;
+document.write("demol" +"<br>").innerHTML=y;
+document.write("demo2").innerHTML=x;
+var location=window.location;
+document.write(location);
+
+
+
+
+
+
+
+
+
